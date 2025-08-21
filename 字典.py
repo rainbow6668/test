08e6e7c284fdd6043d83c02ed1168b15c31966e3
rainbow6668 <2287460646@qq.com>
2025-08-21 10:43:42 +0800

    Add files via upload

---
+++ 字典.py
@@ -0,0 +1,36 @@
+#保存数据
+dic = {"name":"rainbow","age":18}   #{键名：值}
+print(dic)
+
+#查询
+# print(dic["name"])
+# print(dic.get("name"))
+
+#修改
+# dic["age"] = 20
+# print(dic)
+
+# 增加
+dic["tel"] = 177
+print(dic)
+
+#删除
+# del dic["name"]
+# dic.clear()    #清空字典内的内容
+# print(dic)
+# dic.pop("name")
+# print(dic)
+
+# print(len(dic))   #求长度
+
+# print(dic.keys())   #返回键名
+# for i in dic.keys():
+#     print(i)
+
+# print(dic.values())   #返回值
+# for i in dic.values():
+#     print(i)
+
+# print(dic.items())     #以元组形式返回键值对
+# for i in dic.items():
+#     print(i)
